771c7d22733269a2aaf4dc9e80b24607c1b6d422
Tyler Hall <0tillathehun0@gmail.com>
2016-03-16 23:29:41 -0400

    added username/password creation and login

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -39,11 +39,15 @@ angular.module('app.controllers', [])
     function getEmail(authData){
        switch(authData.provider){
             case 'password':
-                return authData.password.email.replace(/\./, ',');
+                return parseEmail(authData.password.email)
             case 'facebook':
-                return authData.facebook.email.replace(/\./, ',');
+                return parseEmail(authData.facebook.email);
         }; 
-    }
+    };
+    
+    function parseEmail(email){
+        return email.replace(/\./, ',');
+    };
     
     var permissions = {
         scope: 'email'
@@ -56,6 +60,18 @@ angular.module('app.controllers', [])
             $state.go('blindLunch.home');
         });
     };
+    
+    $scope.localLogin = function(email, password){
+        firebaseAuth.$authWithPassword({
+            email: parseEmail(email),
+            password: password
+        }).then(function(authData){
+            console.log('logged in as: ' + authData.uid);
+            $state.go('blindLunch.home');
+        }).catch(function(error){
+            console.error('Authentication failed');
+        })
+    };
 }])
   
 .controller('homeCtrl', function($scope) {
@@ -66,7 +82,36 @@ angular.module('app.controllers', [])
 
 })
       
-.controller('signupCtrl', function($scope) {
-
+.controller('signupCtrl', function($scope, firebaseAuth, rootRef, $state) {
+    /**
+     * Create Account with username password
+     */
+    $scope.createAccount = function(name, username, password){
+        firebaseAuth.$createUser({
+            email: username,
+            password: password
+        }).then(function(authData){
+            //create users node
+            console.log(authData);
+            rootRef.child('users').child(authData.uid).set({
+                username: parseEmail(username),
+                name: name,
+                provider: 'local'
+            }, function(error){
+               if(error){
+                 $scope.error = error;  
+               } else {
+                   $state.go('login');
+               }
+            })
+        }).catch(function(error){
+            //error creating user show error to user
+            $scope.error = error;
+        });
+    }
+    
+    function parseEmail(email){
+        return email.replace(/\./, ',');
+    };
 })
  
