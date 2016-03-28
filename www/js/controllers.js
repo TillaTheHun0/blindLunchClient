3e7b0f75f930e7fb77e68dc20b794a416d243ebc
Rodhlann <tjpepper@vwc.edu>
2016-03-27 21:54:27 -0400

    Range Slider Implementation

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -155,4 +155,46 @@ angular.module('app.controllers', [])
         return email.replace(/\./, ',');
     };
 })
- 
+
+.controller('settingsCtrl', function($scope, rootRef) {
+    $scope.radiusSlider = { 
+        value: 10,
+        options: { 
+            floor: 0, 
+            ceil: 50, 
+            step: 5 
+        }
+    };
+                    
+   $scope.priceSlider = { 
+        value: 2,
+        options: { 
+            floor: 1, 
+            ceil: 3, 
+            step: 1 
+        }
+    };
+                         
+    $scope.participantSlider = { 
+        min: 1,
+        max: 5,
+        options: { 
+            floor: 1, 
+            ceil: 10, 
+            step: 1,
+            noSwitching: true
+        }
+    };
+    
+    $scope.ageSlider = { 
+        min: 20,
+        max: 30,
+        options: { 
+            floor: 16, 
+            ceil: 40, 
+            step: 1,
+            noSwitching: true
+        }
+    };
+})
+
